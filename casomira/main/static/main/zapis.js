b8ffeb94dea58417d8a4f0b3e027f21469866275
enjojoy <albina.nikiforova.5670@gmail.com>
2022-12-06 14:59:07 +0100

    zapis validation fixed

--- casomira/main/static/main/zapis.js
+++ casomira/main/static/main/zapis.js
@@ -17,36 +17,55 @@ for (var i=0; i<inputs.length; i++)  {
 }
 }
 
-let person_choosen = [];
-let aircraft_choosen = [];
-console.log("aircraft choosen length" + aircraft_choosen.length);
+// var aircraft_choosen = [];
+// console.log("aircraft choosen length" + aircraft_choosen.length);
+// console.log("person choosen length" + person_choosen.length);
+// console.log(document.querySelectorAll('input[name=person_choosen]:checked'));
 
 
-person_choosen = $(".person").change(function(){
-    var checked_people = document.querySelectorAll('input[name=person_choosen]').is(':checked');
-    console.log(checked_people.length)
-    console.log(checked_people);
-    return checked_people
-})    
+// var person_choosen = [];
+// person_choosen = $("input[name=person_choosen]").change(function(){
+//     var l = document.querySelectorAll('input[name=person_choosen]:checked');
+//     console.log(l.length)
+//     if(l.length === 0){
+//         return [];
+//     }else{
+//         return document.querySelectorAll('input[name=person_choosen]:checked')
+//     }
+// });    
     
+// var person_choosen = [];
 
-aircraft_choosen = $('.aircraft').change(function(){
-    var checked_aircrafts = document.querySelectorAll('input[name=aircraft_choosen]').is(':checked');
-    console.log(checked_aircrafts.length);
-    console.log(checked_aircrafts);
-    return checked_aircrafts;
-    });
+// person_choosen = $("input[name=person_choosen]:checked").change(function(){
+//     console.log(l)
+//     var l = document.querySelectorAll('input[name=person_choosen]:checked');
 
-console.log(aircraft_choosen);
-console.log("aircraft choosen length" + aircraft_choosen.length);
+//     console.log(l.length)
+//     if(l.length === 0){
+//         return [];
+//     }else{
+//         return document.querySelectorAll('input[name=person_choosen]:checked')
+//     }
+// }).get();        
+// console.log(person_choosen)
+
+// aircraft_choosen = $('input[name=aircraft_choosen]:checked').change(function(){
+//     console.log(document.querySelectorAll('input[name=aircraft_choosen]:checked'))
+//     return document.querySelectorAll('input[name=aircraft_choosen]:checked');
+//     }).get();
+
 
 
 zapis_btn = document.getElementById('zapis-btn');
 error = document.getElementById('error');
 
 function zapis(event){
+        p=$("input[name=person_choosen]:checked").get();
+        a=$('input[name=aircraft_choosen]:checked').get();
+        console.log($("input[name=person_choosen]:checked").get());
+        console.log($('input[name=aircraft_choosen]:checked').get());
 
-        if (person_choosen.length === 0 || aircraft_choosen.length === 0 || person_choosen.length < 2  || aircraft_choosen.length < 1){
+        if ( p.length < 2  || a.length < 1){
             if(error.classList.contains('active')){
                 console.log("The requirement is not met!")
                 event.preventDefault();
@@ -55,9 +74,8 @@ function zapis(event){
                     console.log("The requirement is not met!")
                     event.preventDefault();
             }
-        }
-        
-        if(person_choosen.length >= 2  || aircraft_choosen.length >= 1){
+        // }if(person_choosen.length >= 2  && aircraft_choosen.length >= 1){
+        }else{
             if(error.classList.contains('active')){
                 error.classList.remove('active')
                 console.log("Congrats it works")
