f4268e081880977a5f8d3befb3f270588de0af49
enjojoy <albina.nikiforova.5670@gmail.com>
2022-12-06 12:14:33 +0100

    changes in input validations zapis.js

--- casomira/main/forms.py
+++ casomira/main/forms.py
@@ -1,5 +1,15 @@
 from django import forms
+from django.forms import CheckboxSelectMultiple
+from models import Person
 
 
-class PristalForm(forms.Form):
-    landing  = forms.TimeField()
+
+# class ZapisPersonForm(forms.Form):
+#     choices = forms.ModelMultipleChoiceField(
+#         queryset = Person.all()
+#         widget = forms.CheckboxSelectMultiple
+#     )
+
+    # if request.method == "POST":
+    #     form = ZapisPersonForm(request.POST):
+    #     for id in form.cleaned_data['poeple']
--- casomira/main/static/main/zapis.js
+++ casomira/main/static/main/zapis.js
@@ -1,59 +1,77 @@
-$( document ).ready(function() {
-    // $('#personOpt').multiselect({
-    //     columns: 1,
-    //     nonSelectedText : 'Select a person',
-    //     numberDisplayed: 1,
-        
-    // });
-    // $('#aircraftOpt').multiselect({
-    //     columns: 1,
-    //     nonSelectedText : 'Select an airplane',
-    //     numberDisplayed: 1,
+// const zapis_btn = document.querySelector(".btn-zapis");
 
-    // });
+// zapis_btn.addEventListener('click', zapis)
 
-    let person_choosen;
-    let aircraft_choosen;
+// function zapis(event){
+//     console.log('Ypu are trying to sign in')
+//     event.preventDefault();
+// }
 
-    //Returns choosen values
+var inputs = document.getElementsByTagName('input');
 
-    person_choosen =  $('person').change(function(){
-        
-                person_choosen = $(this).val();  // this will give values of selected option by array
-                return person_choosen;
-                
-                });
+function onLoad(){
+for (var i=0; i<inputs.length; i++)  {
+  if (inputs[i].type == 'checkbox')   {
+    inputs[i].checked = false;
+  }
+}
+}
+
+let person_choosen = [];
+let aircraft_choosen = [];
+console.log("aircraft choosen length" + aircraft_choosen.length);
 
 
-    aircraft_choosen = $('aircraft_choosen').change(function(){
-        
-                aircraft_choosen = $(this).val();  // this will give values of selected option by array
-                console.log(aircraft_choosen);
-                return aircraft_choosen;
-                });
-        
+person_choosen = $(".person").change(function(){
+    var checked_people = document.querySelectorAll('input[name=person_choosen]').is(':checked');
+    console.log(checked_people.length)
+    console.log(checked_people);
+    return checked_people
+})    
     
-                zapis_btn = document.getElementById('zapis-btn');
-                error = document.getElementById('error');
+
+aircraft_choosen = $('.aircraft').change(function(){
+    var checked_aircrafts = document.querySelectorAll('input[name=aircraft_choosen]').is(':checked');
+    console.log(checked_aircrafts.length);
+    console.log(checked_aircrafts);
+    return checked_aircrafts;
+    });
+
+console.log(aircraft_choosen);
+console.log("aircraft choosen length" + aircraft_choosen.length);
+
+
+zapis_btn = document.getElementById('zapis-btn');
+error = document.getElementById('error');
+
+function zapis(event){
+
+        if (person_choosen.length === 0 || aircraft_choosen.length === 0 || person_choosen.length < 2  || aircraft_choosen.length < 1){
+            if(error.classList.contains('active')){
+                console.log("The requirement is not met!")
+                event.preventDefault();
+                } else{
+                    error.classList.add('active')
+                    console.log("The requirement is not met!")
+                    event.preventDefault();
+            }
+        }
+        
+        if(person_choosen.length >= 2  || aircraft_choosen.length >= 1){
+            if(error.classList.contains('active')){
+                error.classList.remove('active')
+                console.log("Congrats it works")
+                event.preventDefault();
+            }else{
+                console.log("Congrats it works")
+                event.preventDefault();
+            }
+        }
+}
+        
                 
-                function zapis(){
-                
-                        if (person_choosen === null || aircraft_choosen === null || person_choosen.length < 2  || aircraft_choosen.length < 1){
-                            if(error.classList.contains('active')){
-                                } else{
-                                    error.classList.add('active')
-                            }
-                        }else{
-                            if(error.classList.contains('active')){
-                               error.classList.remove('active')
-                           }else{
-                           }
-                        }
-                }
-                
-                 
-                //Event listeners
-                
-                zapis_btn.addEventListener("click", zapis);
-});
+//Event listeners
+
+zapis_btn.addEventListener("click", zapis);
+
 
