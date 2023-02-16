ea2086290e35e6a1d34e044fb797f4dd9b5970a4
enjojoy <albina.nikiforova.5670@gmail.com>
2023-02-16 09:24:28 +0100

    Everything

--- casomira/frontend/vue.config.js
+++
@@ -1,8 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-
-module.exports = defineConfig({
-  publicPath : "http://127.0.0.1:8080",
-  outputDir : "../static/dist",
-  indexPath : "../../templates/main/entry.html",
-  transpileDependencies: true,
-});
